f76ee412db8f64a9e505fe8743b992145a9c126a
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-07-07 17:14:39 +0100

    Fix(palette): change colors to RGB Add(main.ts): brighten circle clr on click

--- simon/src/js/main.ts
+++ simon/src/js/main.ts
@@ -10,16 +10,29 @@ const userClicksArr: string[] = [];
 let userClickCounter: number = 0
 
 //Null variable handlers
-if(!circles) {
+if (!circles) {
     throw new Error("There is no existing variable circles");
 }
 
 // Functions
 const handleCircleClick = (e: Event) => {
-    console.log(e.target.dataset.circleSize);
+    const targetedCircle = e.target
+    userClicksArr.push(targetedCircle.dataset.circleSize)
+    // console.log("", getComputedStyle(targetedCircle).backgroundColor)
+    brightenColor(targetedCircle)
 }
 
-// // Event listeners
+// Testing this code 
+
+const brightenColor = (targetedCircle: Element) => {
+    console.log(targetedCircle)
+    const circleOriginalColor = getComputedStyle(targetedCircle).backgroundColor
+    const brightColor = targetedCircle.dataset.brightColor
+    targetedCircle.style.backgroundColor = brightColor
+}
+
+//  End of new code
+// Event listeners
 circles.forEach((circle) => {
     circle.addEventListener("click", handleCircleClick)
 })
