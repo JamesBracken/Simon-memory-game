e2f6e6780bc602fe8620fa4386ff77b28b5cacc6
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-07-07 15:46:04 +0100

    Add(main.ts): Refactored all functions, event listeners into 1 individual handler

--- simon/src/js/main.ts
+++ simon/src/js/main.ts
@@ -1,51 +1,25 @@
 import '../styles/main.scss'
 
 // Global variables
+
 // Const
-const smallCircle = document.querySelector<HTMLDivElement>(".game__circle--small");
-const mediumCircle = document.querySelector<HTMLDivElement>(".game__circle--medium");
-const largeCircle = document.querySelector<HTMLDivElement>(".game__circle--large");
-const xLargeCircle = document.querySelector<HTMLDivElement>(".game__circle--xlarge");
-const userClicksArr: number[] = [];
-let userClickCounter: number = 0
+const circles = document.querySelectorAll<HTMLDivElement>(".game__circle")
+const userClicksArr: string[] = [];
+
 // Let
+let userClickCounter: number = 0
 
-// Event listeners
-smallCircle?.addEventListener("click", handleSmallCircleClick)
-mediumCircle?.addEventListener("click", handleMediumCircleClick)
-largeCircle?.addEventListener("click", handleLargeCircleClick)
-xLargeCircle?.addEventListener("click", handleXLargeCircleClick)
+
 // Functions
-
-// Here we are using regular functions instead of arrow functions since 
-// function expressions will not be hoisted
-function handleSmallCircleClick():void{
-    console.log("handleSmallCircleClick")
-    userClicksArr.push(0)
-    userClickCounter+= 1
-    console.log(userClickCounter)
+const handleCircleClick = (e: Event) => {
+    console.log(e.target.dataset.circleSize)
 }
 
-function handleMediumCircleClick():void{
-    console.log("handleMediumCircleClick")
-    userClicksArr.push(1)
-    userClickCounter+= 1
-    console.log(userClickCounter)
-}
+// // Event listeners
+circles.forEach((circle) => {
+    circle.addEventListener("click", handleCircleClick)
+})
 
-function handleLargeCircleClick():void{
-    console.log("handleLargeCircleClick")
-    userClicksArr.push(2)
-    userClickCounter+= 1
-    console.log(userClickCounter)
-}
-
-function handleXLargeCircleClick():void{
-    console.log("handleXLargeCircleClick")
-    userClicksArr.push(3)
-    userClickCounter+= 1
-    console.log(userClickCounter)
-}
 
 // Items which don't conform to other categories
 
